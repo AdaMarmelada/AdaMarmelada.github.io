b09ac4901917a8b3075cf6725ae219a6df2a73f7
kDevelop <kat-kole@mail.ru>
2020-11-23 14:29:13 +0300

    remove dir

--- monitor_audio/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("body").on("click", ".js-toggle-link", function () {
-        var $parent = $(this).parent();
-        $parent.find('.js-toggle-link').removeClass('active');
-        $(this).addClass('active');
-
-        $($(this).attr("data-target")).attr("href", $(this).attr("data-url"));
-    });
-});
